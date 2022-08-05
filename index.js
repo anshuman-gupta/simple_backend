08225a586d135eb2e37311ef2948ed9ce6ad50d1
anshuman <anshumangupta5050@gmail.com>
2022-08-05 09:55:55 +0530

    first commit

--- index.js
+++
@@ -1,9 +0,0 @@
-const express= require("express")
-const app = express()
-
-app.get("/",(req, res)=>{
-    res.send("this is home page")
-})
-
-app.listen(3000, ()=>console.log("server is running"))
-
